c44b5a330392096ecacdfd3d17597e378824275f
DudaRobert <duda_robert@outlook.com>
2026-02-22 13:24:51 +0100

    Initial Commit

---
+++ API/Program.cs
@@ -0,0 +1,19 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
